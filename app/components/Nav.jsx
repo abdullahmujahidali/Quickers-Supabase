3c97ed82d455f91b613391b9ff6a3af585f13b64
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-05-11 18:50:04 +0500

    added changes

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,40 +2,21 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 const Nav = () => {
 
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
-      <Link href='/' className='flex gap-2 flex-center'>
+      <div className='flex-center flex-auto'>
+        <Link href='/' className='flex gap-2 flex-center'>
         <Image
           src='/assets/images/logo.webp'
           alt='logo'
           width={120}
-          height={30}
+          height={40}
           className='object-contain'
         />
       </Link>
-
-      {/* Desktop Navigation */}
-      <div className='sm:flex hidden'>
-
-          <div className='flex gap-3 md:gap-5'>
-            <Link href='/config' className='black_btn'>
-              Update Config
-            </Link>
-          </div>
-
-      </div>
-
-      {/* Mobile Navigation */}
-      <div className='sm:hidden flex relative'>
-          <div className='flex gap-3 md:gap-5'>
-            <Link href='/config' className='black_btn'>
-              Update Config
-            </Link>
-          </div>
       </div>
     </nav>
   );
--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+'use client'
+import Link from "next/link";
+
 
 const Home = () => (
   <section className='mt-20 w-full flex-center flex-col'>
@@ -9,7 +12,13 @@ const Home = () => (
     <p className='mt-20 desc text-center'>
       Q-Config is a free service that allows you to update your Quickers app without republishing.
     </p>
-
+    <div className='mt-8 sm:flex hidden'>
+      <div className='flex gap-3 md:gap-5'>
+        <Link href='/config' className='black_btn'>
+          Update Config
+        </Link>
+      </div>
+    </div>
   </section>
 );
 
